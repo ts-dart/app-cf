b5cca8c3337813d55f758f8f3a71137441bc08f5
thiago <thiagoedusan5.11@outlook.com>
2022-12-29 01:02:56 -0300

    style: estilo componente aside

--- app/src/components/Aside.vue
+++ app/src/components/Aside.vue
@@ -1,24 +1,51 @@
 <template>
-    <aside>
-        <section>
-            <div id="divLogo">
-                <img id="one" src="images/left-arrow.svg" alt="">
-                <img id="two" src="images/right-arrow.svg" alt="">
-                <img id="three" src="images/c-logo.svg" alt="">
-                <img id="four" src="images/a-logo.svg" alt="">
-                <img id="five" src="images/s-logo.svg" alt="">
-                <img id="six" src="images/h-logo.svg" alt="">
-                <img id="seven" src="images/f-logo.svg" alt="">
-                <img id="eight" src="images/o-logo.svg" alt="">
-                <img id="nine" src="images/r-logo.svg" alt="">
-                <img id="ten" src="images/c-logo.svg" alt="">
-                <img id="eleven" src="images/e-logo.svg" alt="">
+    <aside id="sider-financiador">
+        <div id="sider-fornecedor">
+            <div id="menu-hero">
+                <div id="sider-logo">
+                    <div id="Logo">
+                        <div id="Icon">
+                            <img id="one" src="images/left-arrow.svg" alt="">
+                            <img id="two" src="images/right-arrow.svg" alt="">
+                        </div>
+                    </div>
+                    <div id="Wordmark">
+                        <div id="Group1">
+                            <img id="seven" src="images/f-logo.svg" alt="">
+                            <img id="eight" src="images/o-logo.svg" alt="">
+                            <img id="nine" src="images/r-logo.svg" alt="">
+                            <img id="ten" src="images/c-logo.svg" alt="">
+                            <img id="eleven" src="images/e-logo.svg" alt="">
+                        </div>
+                        <div id="Group2">
+                            <img id="three" src="images/c-logo.svg" alt="">
+                            <img id="four" src="images/a-logo.svg" alt="">
+                            <img id="five" src="images/s-logo.svg" alt="">
+                            <img id="six" src="images/h-logo.svg" alt="">
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div id="imgAndNf">
-                <img src="images/hands.svg" alt="">
-                <p>Notas fiscais</p>
+            <div id="menu-item">
+                <div id="menu-item-selected">
+                    <div id="Frame678">
+                        <img id="Rectangle69" src="images/standing-bar.svg" alt="">
+                    </div>
+                    <div id="Frame667">
+                        <div id="Frame679">
+                            <div id="icon-menu-proposta">
+                                <div id="two-for-vector">
+                                    <img id="icon-vector" src="images/hands.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div id="Frame680">
+                            <p id="propostas">Notas fiscais</p> 
+                        </div>
+                    </div>
+                </div>
             </div>
-        </section>
+        </div>
     </aside>
 </template>
 
@@ -29,15 +56,14 @@
 </script>
 
 <style>
-    aside {
+    #sider-financiador {
         position: absolute;
-        background: #FBFDFE;
         width: 288px;
         height: 900px;
         left: 0px;
         top: 0px;
     }
-    section {
+    #sider-fornecedor {
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -48,7 +74,7 @@
         left: 0px;
         top: 0px;
     }
-    #divLogo {
+    #menu-hero {
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -59,40 +85,26 @@
         order: 0;
         flex-grow: 0;
     }
-    #imgAndNf {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        padding: 0px;
+    #sider-logo {
         width: 288px;
-        height: 64px;
-        flex: none;
-        order: 1;
-        flex-grow: 0;
-    }
-    #imgAndNf p {
-        width: 85px;
-        height: 18px;
-        font-family: 'DM Sans';
-        font-style: normal;
-        font-weight: 500;
-        font-size: 14px;
-        line-height: 18px;
-        display: flex;
-        align-items: center;
-        color: #00AD8C;
+        height: 80px;
         flex: none;
         order: 0;
         flex-grow: 0;
     }
-    #imgAndNf img {
-        box-sizing: border-box;
+    #Logo {
+        position: absolute;
+        width: 160px;
+        height: 30px;
+        left: 24px;
+        top: 25px;
+    }
+    #Icon {
         position: absolute;
         left: 0%;
-        right: 0%;
-        top: 20.83%;
-        bottom: 20.57%;
-        border: 1px solid #00AD8C;
+        right: 75.86%;
+        top: 0%;
+        bottom: 0%;
     }
     #one {
         position: absolute;
@@ -108,33 +120,19 @@
         top: 31.64%;
         bottom: 0%;
     }
-    #three {
+    #Wordmark {
         position: absolute;
         left: 27.48%;
-        right: 62.23%;
-        top: 11.68%;
-        bottom: 22.67%;
-    }
-    #four {
-        position: absolute;
-        left: 37.87%;
-        right: 53.74%;
-        top: 33.82%;
-        bottom: 22.65%;
-    }
-    #five {
-        position: absolute;
-        left: 47.21%;
-        right: 45.97%;
-        top: 33.82%;
-        bottom: 22.66%;
-    }
-    #six {
-        position: absolute;
-        left: 55.15%;
-        right: 35.82%;
-        top: 11.5%;
+        right: 0%;
+        top: 10.77%;
         bottom: 10.76%;
+    }
+    #Group1 {
+        position: absolute;
+        left: 64.13%;
+        right: 0%;
+        top: 10.77%;
+        bottom: 10.77%;
     }
     #seven {
         position: absolute;
@@ -170,5 +168,157 @@
         right: 0%;
         top: 33.82%;
         bottom: 22.65%;
+    }
+    #Group2 {
+        position: absolute;
+        left: 27.48%;
+        right: 35.82%;
+        top: 11.5%;
+        bottom: 10.76%;
+    }
+    #three {
+        position: absolute;
+        left: 27.48%;
+        right: 62.23%;
+        top: 11.68%;
+        bottom: 22.67%;
+    }
+    #four {
+        position: absolute;
+        left: 37.87%;
+        right: 53.74%;
+        top: 33.82%;
+        bottom: 22.65%;
+    }
+    #five {
+        position: absolute;
+        left: 47.21%;
+        right: 45.97%;
+        top: 33.82%;
+        bottom: 22.66%;
+    }
+    #six {
+        position: absolute;
+        left: 55.15%;
+        right: 35.82%;
+        top: 11.5%;
+        bottom: 10.76%;
+    }
+    #menu-item {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 0px;
+        width: 288px;
+        height: 64px;
+        flex: none;
+        order: 1;
+        flex-grow: 0;
+    }
+    #menu-item-selected {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 0px;
+        gap: 21px;
+        width: 149px;
+        height: 64px;
+        flex: none;
+        order: 2;
+        flex-grow: 0;
+    }
+    #Frame678 {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 10px 0px;
+        gap: 10px;
+        width: 4px;
+        height: 84px;
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+    }
+    #Rectangle69 {
+        width: 4px;
+        height: 64px;
+        background: #00AD8C;
+        border-radius: 4px;
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+    }
+    #Frame667 {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 0px;
+        gap: 15px;
+        width: 124px;
+        height: 44px;
+        flex: none;
+        order: 1;
+        flex-grow: 0;
+    }
+    #Frame679 {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 10px 0px;
+        gap: 10px;
+        width: 24px;
+        height: 44px;
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+    }
+    #icon-menu-proposta {
+        width: 24px;
+        height: 24px;
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+    }
+    #two-for-vector {
+        position: absolute;
+        left: 0%;
+        right: 0%;
+        top: 20.83%;
+        bottom: 20.57%;
+    }
+    #icon-vector {
+        box-sizing: border-box;
+        position: absolute;
+        left: 0%;
+        right: 0%;
+        top: 20.83%;
+        bottom: 20.57%;
+    }
+    #Frame680 {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 10px 0px;
+        gap: 10px;
+        width: 85px;
+        height: 44px;
+        flex: none;
+        order: 1;
+        flex-grow: 0;
+    }
+    #propostas {
+        width: 85px;
+        height: 18px;
+        font-family: 'DM Sans';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 18px;
+        display: flex;
+        align-items: center;
+        color: #00AD8C;
+        flex: none;
+        order: 0;
+        flex-grow: 0;
     }
 </style>
